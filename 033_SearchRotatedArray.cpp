c229c262e74a3f501c64d678d45250e5b66dabb3
ashishskore <39729082+ashishskore@users.noreply.github.com>
2021-05-04 11:44:46 +0530

    Create 033_SearchRotatedArray.cpp
    Algorithm : 
    take left = v.size() - 1            // -1 is important
    
    loop till left <= right
    calculate mid as, mid = left + (right - left) / 2    // We can  further make it faster as, left + [(right - left) << 1]
    rotation works faster than divide by 2.
    
    check if v[mid] == target if yes return mid, found the target here
    else check if left half is strictly incremental, as v[left] <= v[mid]
                  if Yes, then 
                                  check if target lies in left half as , v[left] <= target <= v[mid]
                                              then update high = mid - 1 
                                   else update low = mid + 1
                   else check if target lies in right half as v[mid] >= target >= v[right]
                                    if Yes then update low = mid + 1
                                    else update right = mid - 1

---
+++ 033_SearchRotatedArray.cpp
@@ -0,0 +1,62 @@
+/*
+There is an integer array nums sorted in ascending order (with distinct values).
+Prior to being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+Given the array nums after the rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+Example 3:
+
+Input: nums = [1], target = 0
+Output: -1
+
+Constraints:
+
+1 <= nums.length <= 5000
+-10^4 <= nums[i] <= 10^4
+All values of nums are unique.
+nums is guaranteed to be rotated at some pivot.
+-10^4 <= target <= 10^4
+*/
+
+class Solution {
+public:
+    //
+    //   5 6 7 8 9 1 2 3 4 
+    int search(vector<int>& arr, int target) {
+        int n = arr.size();
+        int i,j;
+        int l=0,r=n-1;
+        
+        while(l <= r){
+            int m = (l+r)>>1;
+            if(arr[m] == target){
+                return m;
+            }
+            if(arr[l] <= arr[m]){
+                
+                if(arr[l] <= target && target <= arr[m]){
+                    r = m-1;
+                }
+                else{
+                    l = m+1;
+                }
+                
+            }
+            else{
+                if(arr[m] <= target && target <= arr[r]){
+                    l = m+1;
+                }
+                else{
+                    r = m-1;
+                }
+            }
+        }
+        return -1;
+    }
+};
